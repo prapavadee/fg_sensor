54a115f5a075d8236544e609bb0930eb8571c165
prapavadee <43396081+prapavadee@users.noreply.github.com>
2018-09-19 10:46:55 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ExSocN69jkHORbaPMaBT792sNAnon2oZJzIMqwvHuPStzMSayDA6hyzyyj8oMeUdYJIIHZYPXv2g7VZpiiy+s16U/vQyc68+p/zm87ErYv/jwdAES+agTXVTyicf6kWk70AhFjpCrx50au1Q2nsJTwdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
